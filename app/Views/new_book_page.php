c488fdff00c9d9d599ba04b3117d3d82bd6d194e
piturria <patricia.iturriaganez@gmail.com>
2026-03-13 19:56:58 +0100

    minor fix: required only for no null in book table

--- app/Views/new_book_page.php
+++ app/Views/new_book_page.php
@@ -1,13 +1,13 @@
 <?php echo $this->extend('templates/anonymous_layout'); ?>
 <?php echo $this->section('content'); ?>
-    <form action="" method="post">
+    <form action="<?= base_url('books/create'); ?>" method="post">
         <div class="form-group">
             <label for="title">Title:</label>
             <input type="text" class="form-control" id="title" name="title" required>
         </div>
         <div class="form-group">
             <label for="author">Author:</label>
-            <input type="text" class="form-control" id="author" name="author" required>
+            <input type="text" class="form-control" id="author" name="author">
         </div>
         <div class="form-group">
             <label for="description">Description:</label>
@@ -15,11 +15,11 @@
         </div>
         <div class="form-group">
             <label for="published_year">Published Year:</label>
-            <input type="number" class="form-control" id="published_year" name="published_year" required>
+            <input type="number" class="form-control" id="published_year" name="published_year" min="0" max="<?= date('Y'); ?>">
         </div>
         <div class="form-group">
             <label for="path">Path:</label>
-            <input type="text" class="form-control" id="path" name="path" value="books/fake_book.pdf"required>
+            <input type="text" class="form-control" id="path" name="path" value="books/fake_book.pdf" required>
         </div>
         <button type="submit" class="btn btn-new">Create Book</button>
     </form>
