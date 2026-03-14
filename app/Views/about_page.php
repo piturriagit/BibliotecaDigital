23bbc62e155f6457eb15e8ca5a9373f7ed58a20c
piturria <patricia.iturriaganez@gmail.com>
2026-03-14 07:02:10 +0100

    minor fix

--- app/Views/about_page.php
+++ app/Views/about_page.php
@@ -6,28 +6,26 @@
 </div>
 
 <h2 style="text-align:center">Our Team</h2>
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="<?= base_url('assets/images/profile_211708134_400.png') ?>" alt="Patricia" style="width:100%">
-      <div class="container">
-        <h2>Patricia Iturriaga</h2>
-        <p class="title">Developer</p>
-        <p>Telecommunication Engineer with experience in Quality Assurance.</p>
-        <p>iturriaga.patricia@icloud.com</p>
-        <p><button class="btn btn-contact">Contact</button></p>
-      </div>
+<div class="column">
+<div class="card">
+    <img src="<?= base_url('assets/images/profile_211708134_400.png') ?>" alt="Patricia" style="width:100%">
+    <div class="container">
+    <h2>Patricia Iturriaga</h2>
+    <p class="title">Developer</p>
+    <p>Telecommunication Engineer with experience in Quality Assurance.</p>
+    <p>iturriaga.patricia@icloud.com</p>
+    <p><button class="btn btn-contact">Contact</button></p>
     </div>
-  </div>
-  <div class="column">
-    <div class="card">
-      <img src="assets/images/profile_default_400.png" alt="Patricia" style="width:100%">
-      <div class="container">
-        <h2>Unassigned</h2>
-        <p class="title">QA Engineer</p>
-        <p><button class="btn btn-contact">Contact</button></p>
-      </div>
+</div>
+</div>
+<div class="column">
+<div class="card">
+    <img src="assets/images/profile_default_400.png" alt="Patricia" style="width:100%">
+    <div class="container">
+    <h2>Unassigned</h2>
+    <p class="title">QA Engineer</p>
+    <p><button class="btn btn-contact">Contact</button></p>
     </div>
-  </div>
+</div>
 </div>
 <?php echo $this->endSection(); ?>
